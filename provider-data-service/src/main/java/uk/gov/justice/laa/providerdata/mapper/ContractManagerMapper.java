d62961635cd030ffe5d9e5036f81fafa391f1e04
David Conneely <899881+dconneely@users.noreply.github.com>
2026-03-25 17:14:45 +0000

    fix: Add guid (and other fields) to the contract-manager responses

--- provider-data-service/src/main/java/uk/gov/justice/laa/providerdata/mapper/ContractManagerMapper.java
+++ provider-data-service/src/main/java/uk/gov/justice/laa/providerdata/mapper/ContractManagerMapper.java
@@ -1,7 +1,5 @@
 package uk.gov.justice.laa.providerdata.mapper;
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import uk.gov.justice.laa.providerdata.entity.ContractManagerEntity;
@@ -21,10 +19,7 @@ public interface ContractManagerMapper {
    * @param entity the contract manager entity to convert
    * @return the corresponding OfficeContractManagerV2 DTO
    */
-  @BeanMapping(builder = @Builder(disableBuilder = true), ignoreByDefault = true)
-  @Mapping(target = "contractManagerId")
-  @Mapping(target = "firstName")
-  @Mapping(target = "lastName")
+  @Mapping(target = "email", ignore = true)
   OfficeContractManagerV2 toOfficeContractManagerV2(ContractManagerEntity entity);
 
   /**
@@ -34,9 +29,6 @@ public interface ContractManagerMapper {
    * @param entity the contract manager entity to convert
    * @return the corresponding ContractManagerV2 DTO
    */
-  @BeanMapping(builder = @Builder(disableBuilder = true), ignoreByDefault = true)
-  @Mapping(target = "contractManagerId")
-  @Mapping(target = "firstName")
-  @Mapping(target = "lastName")
+  @Mapping(target = "email", ignore = true)
   ContractManagerV2 toContractManagerV2(ContractManagerEntity entity);
 }

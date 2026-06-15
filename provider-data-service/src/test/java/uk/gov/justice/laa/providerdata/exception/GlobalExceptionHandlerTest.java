14182a30785a98ec5e42206c78d5cc68b387329b
David Conneely <899881+dconneely@users.noreply.github.com>
2026-06-15 09:55:40 +0100

    chore: Update actions, dependencies, plugins (#145)

--- provider-data-service/src/test/java/uk/gov/justice/laa/providerdata/exception/GlobalExceptionHandlerTest.java
+++ provider-data-service/src/test/java/uk/gov/justice/laa/providerdata/exception/GlobalExceptionHandlerTest.java
@@ -10,8 +10,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.ProblemDetail;
 import uk.gov.justice.laa.providerdata.model.ErrorResponseError;
-import uk.gov.justice.laa.providerdata.model.InternalServerErrorError;
-import uk.gov.justice.laa.providerdata.model.NotFoundErrorError;
 
 class GlobalExceptionHandlerTest {
 
@@ -24,7 +22,7 @@ class GlobalExceptionHandlerTest {
 
     assertThat(result.getStatus()).isEqualTo(NOT_FOUND.value());
     assertThat(result.getProperties()).containsKey("error");
-    assertThat(((NotFoundErrorError) result.getProperties().get("error")).getErrorCode())
+    assertThat(((ErrorResponseError) result.getProperties().get("error")).getErrorCode())
         .isEqualTo("P00NF");
   }
 
@@ -59,7 +57,7 @@ class GlobalExceptionHandlerTest {
 
     assertThat(result.getStatus()).isEqualTo(INTERNAL_SERVER_ERROR.value());
     assertThat(result.getProperties()).containsKey("error");
-    assertThat(((InternalServerErrorError) result.getProperties().get("error")).getErrorCode())
+    assertThat(((ErrorResponseError) result.getProperties().get("error")).getErrorCode())
         .isEqualTo("P00SE");
   }
 }
